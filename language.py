36f0dbbe074db45bec11f9b4709b3d6e46485208
Antua <suleykin2005@mail.ru>
2024-11-14 18:46:02 +0300

    processing path

--- language.py
+++ language.py
@@ -1,8 +1,17 @@
 import sys
+import os
 
 
 def main(path_to_itog_file):
+    directory = os.path.dirname(path_to_itog_file)
+    if not os.path.exists(directory) or not os.path.isdir(directory):
+        return
+    with open(path_to_itog_file, mode="w") as f:
+        f.write("12331")
     pass
+
+
+
 
 
 
@@ -13,5 +22,5 @@ if __name__ == "__main__":
     # #     print("Аргументы указаны неверно")
     # #     exit()
     # path_to_itog_file = sys.argv[1]
-    path_to_itog_file = ''
+    path_to_itog_file = r'C:\Users\Antua\PycharmProjects\config3\testfile.txt'
     main(path_to_itog_file)
