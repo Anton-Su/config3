2d876d7564e9d65dbf59e3548c4db873473646ea
Antua <suleykin2005@mail.ru>
2024-11-20 21:41:23 +0300

    vvod secured

--- language.py
+++ language.py
@@ -3,6 +3,7 @@ import os
 import keyboard
 import time
 import threading
+import re
 
 
 def read_input(text):
@@ -26,9 +27,33 @@ def main(path_to_itog_file):
 
 
 def parse(text):
-    for i in text:
-        for symbol in i:
-            print(symbol)
+    dict = {}
+    for line in text:
+        line = line.strip()  # учебный язык не позволяет заглавную первую
+        if re.fullmatch("\S+\.\s*=\s*\S+", line):
+            print("Empty bare keys are not allowed")
+            break
+        if re.fullmatch(".+\s?=", line):
+            print("Empty value")
+            break
+        if re.fullmatch("[\w][_a-zA-Z0-9]*(\s*\"?\.\s*\"?[_a-zA-Z0-9]+)*\s*=\s*[^=]+", line):  # объявление переменной
+            name, key = line.split("=")
+            while " " in name:
+                name = name.replace(" ", "")
+            key = key.strip()
+            print(name)
+            print(key)
+            if key in dict:
+                print("Can't redefine existing key")
+                break
+        else:
+            print("INVALIDE CHARACTER")
+            break
+        # комментарии
+        # for symbol in range(len(line)):
+        #     if symbol == "'":
+        #
+        #     print(symbol)
 
 
 def write(path_to_itog_file, text):  # где-то уже в конце
